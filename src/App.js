25673c5b7ab9f01ed8b39385310fedf3b662cb94
D-coder <adi_veer@hotmail.com>
2021-03-12 22:56:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-
-import './App.scss';
-import TextField from '@material-ui/core/TextField';
-import { useState,useEffect } from 'react';
+import "./App.scss";
+import TextField from "@material-ui/core/TextField";
+import { useState, useEffect } from "react";
 import { Button } from "@material-ui/core";
+
+
+
+
 import { db } from "./firebase_config";
 import firebase from "firebase";
 
 import TodoListItem from "./Todo";
 
 function App() {
-
-
   const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState("");
 
   useEffect(() => {
     getTodos();
-  }, []); // blank to run only on first launch
+  }, []); // blank to run only on first launch\
 
+
+  
   function getTodos() {
     db.collection("todos").onSnapshot(function (querySnapshot) {
       setTodos(
@@ -32,21 +35,21 @@ function App() {
 
   function addTodo(e) {
     e.preventDefault();
-    if(todoInput.trim() !==""){
-    db.collection("todos").add({
-      inprogress: true,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      todo: todoInput,
-    });
-  }
+    if (todoInput.trim() !== "") {
+      db.collection("todos").add({
+        inprogress: true,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        todo: todoInput,
+      });
+    }
     setTodoInput("");
   }
   return (
     <div className="App">
       <div className="layout">
-      <h1>MyTask Manager - 📝</h1>
-      <form>
-      <TextField
+        <h1>MyTask Manager - 📝</h1>
+        <form>
+          <TextField
             id="standard-basic"
             label="Plans for the day?"
             value={todoInput}
@@ -57,12 +60,13 @@ function App() {
             type="submit"
             variant="contained"
             onClick={addTodo}
-            style={{ display: "none" }}
+            style={{ display: "" }}
+
           >
-            Default
+            Add
           </Button>
-      </form>
-      <div className="listitems">
+        </form>
+        <div className="listitems">
           {todos.map((todo) => (
             <TodoListItem
               todo={todo.todo}
@@ -71,9 +75,21 @@ function App() {
             />
           ))}
         </div>
-        <div id="alerts">
+
        
+      <div className="site-footer">
+          Developed by: Aditya, Iftikhar, Olivia
+         
         </div>
+  
+ 
+
+
+
+
+
+
+        
       </div>
     </div>
   );
